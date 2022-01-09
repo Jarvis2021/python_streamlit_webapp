ffff39a9007c104b41412cb211f9ac889dc99b51
Pramod Kumar <Pramod.Voola@gmail.com>
2022-01-09 11:20:09 -0500

    added Charts and histogram using numpy and plotly libraries

--- Project/app.py
+++ Project/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
+import plotly.express as px
 
 DATA_URL = ('/Users/Pramod/Github/python_streamlit_webapp/Project/Motor_Vehicle_Collisions_Crashes.csv')
 
@@ -18,6 +19,7 @@ def load_data (nrows):
     return data
 
 data = load_data(100000)
+original_data = data
 
 st.header("Where are the most people injured most in NYC?")
 injured_people = st.slider("Number of persons injured in a vehicle collisions", 0, 19) # 19 - Number of max persons injured at a given spot
@@ -53,6 +55,28 @@ st.write(pdk.Deck(
         ),
     ]
 ))
+
+st.subheader("Breakdown by minute between %i:00 and %i:00" %(hour,(hour+1) %24))
+filtered = data[
+    (data['date/time'].dt.hour >= hour) & (data['date/time'].dt.hour < (hour +1))
+]
+
+hist = np.histogram(filtered['date/time'].dt.minute, bins=60, range=(0,60))[0]
+chart_data = pd.DataFrame({'minute': range(60), 'crashes':hist })
+fig = px.bar(chart_data, x='minute', y='crashes', hover_data=['minute', 'crashes'], height= 400)
+st.write(fig)
+
+st.header("Top 5 dangerous streets by affected type")
+select = st.selectbox('Affected type of people', ['Pedestrians', 'Cyclists', 'Motorists'])
+
+if select == 'Pedestrians':
+    st.write(original_data.query("injured_pedestrians >=1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how='any')[:5])
+
+elif select == 'Cyclists':
+    st.write(original_data.query("injured_cyclists >=1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending=False).dropna(how='any')[:5])
+
+else:
+    st.write(original_data.query("injured_motorists >=1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how='any')[:5])
 
 
 
